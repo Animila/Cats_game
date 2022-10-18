c61adb70643e715434bbe037c55336a802540670
animila <hristoforovila5@gmail.com>
2022-10-19 01:52:05 +0900

    Сделал механику 'похолодания', поработал с окнами

--- Assets/Scripts/UI/UIScene.cs
+++ Assets/Scripts/UI/UIScene.cs
@@ -2,14 +2,39 @@
 using UnityEngine.SceneManagement;
 
 public class UIScene : MonoBehaviour
+
 {
+	public void Awake()
+	{
+		Time.timeScale = 1f;
+	}
+	[SerializeField] private GameObject pause;
 	public void ChangeScene(string name)
 	{
 		SceneManager.LoadScene(name);
 	}
 
+	public void SetModal(bool status, GameObject modal)
+	{
+		Time.timeScale = 0f;
+		modal.SetActive(status);
+	}
+	public void CloseModal(GameObject modal)
+	{
+		Time.timeScale = 1f;
+		modal.SetActive(false);
+	}
+
 	public void Exit()
 	{
 		Application.Quit();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKey(KeyCode.F12))
+		{
+			SetModal(true, pause);
+		}
 	}
 }
---
+++ Assets/Scripts/cats/Temperature.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Temperature : MonoBehaviour
+{
+	[SerializeField] private float minTemp = -45f;
+	[SerializeField] private float maxTemp = 25f;
+	[SerializeField] private float temp = 25f;
+	[SerializeField] private Slider slider;
+
+	[SerializeField] private UIScene scenes;
+	[SerializeField] private GameObject gameover;
+	private void Update()
+	{
+		temp -= 10 * Time.deltaTime;
+		slider.value = temp;
+		if (minTemp >= temp)
+		{
+			Time.timeScale = 0f;
+			scenes.SetModal(true, gameover);
+		}
+	}
+}

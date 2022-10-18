64f774a8fc5b63a04b0d41abdffb8f3c789403e8
animila <hristoforovila5@gmail.com>
2022-10-19 01:06:50 +0900

    Добавил бег и усталость

--- Assets/Scripts/cats/Move.cs
+++ Assets/Scripts/cats/Move.cs
@@ -1,18 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Move : MonoBehaviour
 {
-	[SerializeField] private float Force;
+	[SerializeField] private float Force = 5;
 
 	[SerializeField] private CharacterController controller;
 
 	[SerializeField] private float smoothTime;
 
 	[SerializeField] private Transform firstCamera;
+
+	[SerializeField] private float food = 100;
+	[SerializeField] private Slider slider;
 	private float smooth;
+
+	public void setFood(float food)
+	{
+		this.food = food;
+	}
 
 	private void Update()
 	{
+		slider.value = food;
 		float horizontal = Input.GetAxisRaw("Horizontal");
 		float vertical = Input.GetAxisRaw("Vertical");
 		Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -25,8 +35,21 @@ public class Move : MonoBehaviour
 			Vector3 move = Quaternion.Euler(0f, rotationAngle, 0f) * Vector3.forward;
 			controller.Move(move.normalized * Force * Time.deltaTime);
 		}
-		if (Input.GetKey(KeyCode.W))
+		if (Input.GetKey(KeyCode.LeftShift) && food >= 0)
 		{
+			Force = 10;
+			food -= 1;
+		}
+		else
+		{
+			Force = 5;
+			food += 0.3f;
+		}
+
+		if (food >= 100)
+		{
+			food = 100;
+
 		}
 	}
 }
